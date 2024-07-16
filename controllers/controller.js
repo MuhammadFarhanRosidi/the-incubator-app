40de781d32c0de5c87491900264c22a6125cefc3
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2024-07-16 18:27:26 +0700

    revisi controller handler edit start up

--- controllers/controller.js
+++ controllers/controller.js
@@ -115,7 +115,8 @@ class Controller {
                     valuation
                 },
                 { where : {
-                    IncubatorId : incubatorId
+                    IncubatorId : incubatorId,
+                    id : startUpId
                     }
                 }
             )
